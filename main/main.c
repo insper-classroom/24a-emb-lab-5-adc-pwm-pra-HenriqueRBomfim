59ec96fbd02c434e7bbf1d979541386265920763
henriquerbomfim <henriquerb1@al.insper.edu.br>
2024-04-03 17:16:13 +0000

    Código pronto

--- main/main.c
+++ main/main.c
@@ -15,56 +15,55 @@
 
 QueueHandle_t xQueueAdc;
 
+#define deadZone 150
+
 typedef struct adc {
     int axis;
     int val;
 } adc_t;
 
 void x_task(void *p) {
+    adc_t data;
     adc_init();
-    adc_gpio_init(27);
+    adc_gpio_init(26);
 
-    // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
-    const float conversion_factor = 3.3f / (1 << 12);
-
-    uint16_t result;
     while (1) {
-        adc_select_input(1); // Select ADC input 1 (GPIO27)
-        result = adc_read();
-        result = ((result * conversion_factor)-2047)/8;
-        if (result > -30 && result < 30){
+        adc_select_input(0); // Select ADC input 0 (GPIO26)
+        float result = adc_read();
+
+        result = result - 2048;
+        result = result / 8;
+
+        if (abs(result) < deadZone) {
             result = 0;
         }
-        printf("Valor em x: %d V\n", result);
-        adc_t data;
-        data.axis = 1;
+
         data.val = result;
-        xQueueSend(xQueueAdc, &data, 100);
-        vTaskDelay(pdMS_TO_TICKS(200));
+        data.axis = 1;
+        xQueueSend(xQueueAdc, &data, portMAX_DELAY);
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
 
 void y_task(void *p) {
+    adc_t data;
     adc_init();
-    adc_gpio_init(26);
+    adc_gpio_init(27);
 
-    // 12-bit conversion, assume max value == ADC_VREF == 3.3 V
-    const float conversion_factor = 3.3f / (1 << 12);
-
-    uint16_t result;
     while (1) {
-        adc_select_input(0); // Select ADC input 0 (GPIO26)
-        result = adc_read();
-        result = ((result * conversion_factor)-2047)/8;
-        if (result > -30 && result < 30){
+        adc_select_input(1); // Select ADC input 1 (GPIO27)
+        float result = adc_read();
+
+        result = result - 2048;
+        result = result / 8;
+
+        if (abs(result) < deadZone) {
             result = 0;
         }
-        printf("Valor em y: %d V\n", result);
-        adc_t data;
-        data.axis = 0;
         data.val = result;
-        xQueueSend(xQueueAdc, &data, 100);
-        vTaskDelay(pdMS_TO_TICKS(200));
+        data.axis = 0;
+        xQueueSend(xQueueAdc, &data, portMAX_DELAY);
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
 }
 
@@ -95,8 +94,8 @@ int main() {
     xQueueAdc = xQueueCreate(32, sizeof(adc_t));
 
     xTaskCreate(uart_task, "uart_task", 4096, NULL, 1, NULL);
-    xTaskCreate(x_task, "x_task", 4095, NULL, 1, NULL);
-    xTaskCreate(y_task, "y_task", 4095, NULL, 1, NULL);
+    xTaskCreate(x_task, "adc_task", 4096, NULL, 1, NULL);
+    xTaskCreate(y_task, "adc_task2", 4096, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
